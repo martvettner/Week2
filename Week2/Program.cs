ff3448820504874c92e3ef866ebbfd9cc08923fd
Opilane TTHK <opilane@tthk.ee>
2020-09-07 13:59:52 +0300

    fix

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -9,12 +9,12 @@ namespace Week2
             Console.WriteLine("Mis te nimi on?");
             string name = Console.ReadLine();
             Console.WriteLine("Tere " + name);
-            Console.WriteLine("Mis aastal sa sündisid?");
+            Console.WriteLine("Kui vana sa oled?");
             string userInput = Console.ReadLine();
             int dateOfBirthInt;
             dateOfBirthInt = Int32.Parse(userInput);
             int dobCalculated = 2020 - dateOfBirthInt;
-            Console.WriteLine("Sa oled " + dobCalculated + " aastat vana");
+            Console.WriteLine("Sa sündisid " + dobCalculated + " aastal.");
                 
               
         }

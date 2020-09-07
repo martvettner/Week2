dcd1184989c62ebeddf2fd0c50d3ade0fb950ccf
Opilane TTHK <opilane@tthk.ee>
2020-09-07 13:53:15 +0300

    first

---
+++ Week2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis te nimi on?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Tere " + name);
+            Console.WriteLine("Mis aastal sa sündisid?");
+            string userInput = Console.ReadLine();
+            int dateOfBirthInt;
+            dateOfBirthInt = Int32.Parse(userInput);
+            int dobCalculated = 2020 - dateOfBirthInt;
+            Console.WriteLine("Sa oled " + dobCalculated + " aastat vana");
+                
+              
+        }
+    }
+}
